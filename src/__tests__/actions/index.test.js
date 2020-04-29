6eaeb9e2e1ea549aee561b639850ea0f8348fe37
Michelle Morin <morin2m@gmail.com>
2020-04-29 15:42:40 -0700

    test remaining two action creators
    Co-authored-by: Geoff Goetz <59552871+Pieharder@users.noreply.github.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -6,4 +6,15 @@ describe('hangman actions', () => {
       type: 'SET_ANSWER'
     });
   });
+  it('addMistake should create ADD_MISTAKE action', () => {
+    expect(actions.addMistake()).toEqual({
+      type: 'ADD_MISTAKE'
+    });
+  });
+  it('addGuess should create ADD_guess action', () => {
+    expect(actions.addGuess('e')).toEqual({
+      type: 'ADD_GUESS',
+      value: 'e'
+    });
+  });
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,10 @@
 export const setAnswer = () => ({
   type: 'SET_ANSWER'
 });
+export const addMistake = () => ({
+  type: 'ADD_MISTAKE'
+});
+export const addGuess = (value) => ({
+  type: 'ADD_GUESS',
+  value: value
+});
