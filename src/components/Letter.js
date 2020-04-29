8b4d8f43ae26028f213d40fbf15aeab3ec1f7f5d
Michelle Morin <morin2m@gmail.com>
2020-04-29 11:54:21 -0700

    update props and function for guessing letter
    Co-authored-by: Geoff Goetz <59552871+Pieharder@users.noreply.github.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 function Letter(props){
+  const buttonStyles = {
+    margin: '2px'
+  };
+
   return (
     <React.Fragment>
-      <Button variant="outline-secondary">{props.currentLetter}</Button>
+      <Button style={buttonStyles} onClick={()=> props.whenLetterClicked(props.currentLetter)} variant="outline-secondary">{props.currentLetter}</Button>
     </React.Fragment>
-  )
+  );
+}
+
+Letter.propTypes = {
+  currentLetter: PropTypes.string,
+  id: PropTypes.string,
+  whenLetterClicked: PropTypes.func
 }
 
 export default Letter;
--- src/components/LetterList.js
+++ src/components/LetterList.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import Letter from './Letter';
+import PropTypes from 'prop-types';
 
-function LetterList() {
+function LetterList(props) {
 
-  const masterLetterList = ['a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  const masterLetterList = {
+    1: 'a', 
+    2: 'b',
+    3: 'c',
+    4: 'd',
+    5: 'e',
+    6: 'f',
+    7: 'g',
+    8: 'h',
+    9: 'i',
+    10: 'j',
+    11: 'k',
+    12: 'l',
+    13: 'm',
+    14: 'n',
+    15: 'o',
+    16: 'p',
+    17: 'q',
+    18: 'r',
+    19: 's',
+    20: 't',
+    21: 'u',
+    22: 'v',
+    23: 'w',
+    24: 'x',
+    25: 'y',
+    26: 'z'
+  };
+
+  const letterStyles = {
+    width: '350px',
+    height: 'auto',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'flexStart',
+    marginTop: '1%',
+    marginBottom: '1%'
+  };
 
   return(
     <React.Fragment>
-      {masterLetterList.map((letter) => 
-        <Letter currentLetter={letter} />
-      )}
+      <div style={letterStyles}>
+        {Object.keys(masterLetterList).map((key) => 
+          <Letter currentLetter={masterLetterList[key]} whenLetterClicked={props.onLetterClick} id={key} />
+        )}
+      </div>
     </React.Fragment>
   );
 };
+
+LetterList.propTypes = {
+  onLetterClick: PropTypes.func
+}
 
 export default LetterList;
