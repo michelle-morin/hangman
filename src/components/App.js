e0cdbd038ce0006ed54a83bf02591fe8410220bd
Michelle Morin <morin2m@gmail.com>
2020-04-29 11:53:16 -0700

    update font styles and remove unused stylesheet
    Co-authored-by: Geoff Goetz <59552871+Pieharder@users.noreply.github.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import '../App.css';
+import Header from './Header';
+import Hangman from './Hangman';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <React.Fragment>
 
-    </React.Fragment>
+  const appStyles = {
+    fontFamily: "'Montserrat', sans-serif"
+  };
+
+  return (
+    <div style={appStyles}>
+      <Header />
+      <Hangman />
+    </div>
   );
 }
 
