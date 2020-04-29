be6972f74d512597106927c6d97fc3b302743470
Michelle Morin <morin2m@gmail.com>
2020-04-29 09:55:38 -0700

    add header component
    Co-authored-by: Geoff Goetz <59552871+Pieharder@users.noreply.github.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+function Header() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar.Brand href="#">Hangman</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  </Navbar>
+  );
+}
+
+export default Header;
