39ac968b920d0c59d4990f67a2433be3d06a74e6
Michelle Morin <morin2m@gmail.com>
2020-04-30 10:23:41 -0700

    add action constants
    Co-authored-by: Geoff Goetz <59552871+Pieharder@users.noreply.github.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,5 @@
+export const SET_ANSWER = 'SET_ANSWER';
+export const ADD_MISTAKE = 'ADD_MISTAKE';
+export const ADD_GUESS = 'ADD_GUESS';
+export const RESET_GUESSES = 'RESET_GUESSES';
+export const RESET_MISTAKES = 'RESET_MISTAKES';
