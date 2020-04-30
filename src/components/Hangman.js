a79775297f2b7c9288a213a560c168214dc2abe8
Michelle Morin <morin2m@gmail.com>
2020-04-30 10:11:55 -0700

    add images to separate component

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { Button, Container } from 'react-bootstrap';
+import { images } from './HangmanImages';
 import LetterList from './LetterList.js';
 import PropTypes from 'prop-types';
-import step0 from '../img/0.png';
-import step1 from '../img/1.png';
-import step2 from '../img/2.png';
-import step3 from '../img/3.png';
-import step4 from '../img/4.png';
-import step5 from '../img/5.png';
-import step6 from '../img/6.png';
 import { connect } from 'react-redux';
 import * as a from './../actions';
 
 function Hangman(props) {
 
   const { dispatch } = props;
-
-  const images = [step0, step1, step2, step3, step4, step5, step6];
 
   function guessedWord(){
     // display either letter if the letter was guessed, or display underscore.
---
+++ src/components/HangmanImages.js
@@ -0,0 +1,9 @@
+import step0 from '../img/0.png';
+import step1 from '../img/1.png';
+import step2 from '../img/2.png';
+import step3 from '../img/3.png';
+import step4 from '../img/4.png';
+import step5 from '../img/5.png';
+import step6 from '../img/6.png';
+
+export const images = [step0, step1, step2, step3, step4, step5, step6];
