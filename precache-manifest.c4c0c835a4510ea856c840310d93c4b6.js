10b471edcde98e897cfc10d3cf1ff5febbc94271
Michelle Morin <morin2m@gmail.com>
2020-04-30 13:18:01 -0700

    Updates

---
+++ precache-manifest.c4c0c835a4510ea856c840310d93c4b6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b6ff8a19d78f0b0504e2c459b4c401",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "949a79a36996ad1b67f7",
+    "url": "/hangman/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "bac79e755c7f39f062ad",
+    "url": "/hangman/static/css/main.32aadc3b.chunk.css"
+  },
+  {
+    "revision": "949a79a36996ad1b67f7",
+    "url": "/hangman/static/js/2.ea8579aa.chunk.js"
+  },
+  {
+    "revision": "bac79e755c7f39f062ad",
+    "url": "/hangman/static/js/main.536eb233.chunk.js"
+  },
+  {
+    "revision": "97cbce59a917d27137a8",
+    "url": "/hangman/static/js/runtime-main.027556ea.js"
+  },
+  {
+    "revision": "f24e0265dcd47b5ff70357a017820f22",
+    "url": "/hangman/static/media/0.f24e0265.png"
+  },
+  {
+    "revision": "00c6464d3fbf5f1cfda25d503896f653",
+    "url": "/hangman/static/media/1.00c6464d.png"
+  },
+  {
+    "revision": "c023a15384bfb1b29622a54bff4880c6",
+    "url": "/hangman/static/media/2.c023a153.png"
+  },
+  {
+    "revision": "9d545e726cb484c097e1787a48fd2365",
+    "url": "/hangman/static/media/3.9d545e72.png"
+  },
+  {
+    "revision": "dffcfbc750511477a1e17c43ae7e40b6",
+    "url": "/hangman/static/media/4.dffcfbc7.png"
+  },
+  {
+    "revision": "f6761f09bc605e358f3dbf1c2bb80e13",
+    "url": "/hangman/static/media/5.f6761f09.png"
+  },
+  {
+    "revision": "5f1fe92d2121d8652272d4e043b941b7",
+    "url": "/hangman/static/media/6.5f1fe92d.png"
+  }
+]);
